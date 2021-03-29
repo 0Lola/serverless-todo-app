b019db04d1aab2da3b0a91ef8771c0c25bf9f504
Lola <zxa011023@gmail.com>
2021-03-29 08:40:58 +0800

    add: secret

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,24 +1,27 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-
+import * as middy from 'middy'
 import { verify } from 'jsonwebtoken'
-// import { verify, decode } from 'jsonwebtoken'
+import { secretsManager } from 'middy/middlewares'
+
 // import { createLogger } from '../../utils/logger'
 // import Axios from 'axios'
-// import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+import { parseUserId } from '../../auth/utils'
 
 // const logger = createLogger('auth')
 
 // Auth0 JSON Web Key Set
 // const jwksUrl = 'https://udacity-serverless-zxa011023.us.auth0.com/.well-known/jwks.json'
-const secret = 'o0kvHInIfOxAxK7nMkGIfBJ739FV6_2hCOWKudEcw-oT399uGGfzA0wCyHPYCOEN'
+// const secret = 'o0kvHInIfOxAxK7nMkGIfBJ739FV6_2hCOWKudEcw-oT399uGGfzA0wCyHPYCOEN'
+const secretId = process.env.AUTH_0_SECRET_ID
+const secretField = process.env.AUTH_0_SECRET_FIELD
 
-export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
+export const handler = middy(async (event: CustomAuthorizerEvent, context): Promise<CustomAuthorizerResult> => {
     console.log('Authorizing a user', event.authorizationToken)
 
     try {
-        const jwtToken = await verifyToken(event.authorizationToken)
+      const jwtToken = await verifyToken(event.authorizationToken,context.AUTH0_SECRET[secretField])
         console.log('User was authorized', jwtToken)
 
         return {
@@ -51,9 +54,9 @@ export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAutho
             }
         }
     }
-}
+})
 
-async function verifyToken(authHeader: string): Promise<JwtPayload> {
+async function verifyToken(authHeader: string, secret: string): Promise<JwtPayload> {
     if (!authHeader) throw new Error('No authentication header')
 
     if (!authHeader.toLowerCase().startsWith('bearer '))
@@ -61,8 +64,21 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
 
     const split = authHeader.split(' ')
     const token = split[1]
-    // const jwt: Jwt = decode(token, { comxplete: true }) as Jwt
-    console.log('verifyToken: ', token)
+    const userId = parseUserId(token)
+    console.log('verifyToken userId: ', userId)
 
     return verify(token, secret, { algorithms: ['HS256'] }) as JwtPayload
 }
+
+handler.use(
+    secretsManager({
+      awsSdkOptions: { region: 'us-east-1' },
+      cache: true,
+      cacheExpiryInMillis: 60000,
+      // Throw an error if can't read the secret
+      throwOnFailedCall: true,
+      secrets: {
+        AUTH0_SECRET: secretId
+      }
+    })
+  )
